dba595eb2b70a611033be034ba23d3b4f3cb947c
guillermo <guillermosamboni0912@gmail.com>
2021-10-15 08:33:17 -0500

    init

---
+++ app/src/main/java/com/senasoft2021new/senasoft2021new/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.senasoft2021new.senasoft2021new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/senasoft2021new/senasoft2021new/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.senasoft2021new.senasoft2021new.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
